429908b09b952e583cd0b901d5f6b1326e976670
Marcus Vinicius Dos Santos Gomes <marcudos@c1r5p3.42sp.org.br>
2024-11-27 21:07:46 -0300

    Removido arquivo na francinette

---
+++ Makefile
@@ -0,0 +1,71 @@
+NAME = libftprintf.a
+# compilador e flags
+CC = cc
+CFLAGS = -Wall -Wextra -Werror -Iinclude
+# dirs
+LIBFT_DIR = libft
+SRC_DIR = src
+OBJ_DIR = obj
+APPS_DIR = apps
+BIN_DIR = bin
+#files
+SRCS = $(wildcard $(SRC_DIR)/*.c)
+OBJS = $(SRCS:$(SRC_DIR)/%.c=$(OBJ_DIR)/%.o)
+LIBFT = $(LIBFT_DIR)/libft.a
+
+# Mandatório
+all: $(LIBFT) $(NAME) 
+
+$(LIBFT):
+	$(MAKE) -C $(LIBFT_DIR)
+
+$(NAME): $(OBJS) $(LIBFT)
+	ar rcs $(NAME) $(OBJS) $(LIBFT)
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(OBJ_DIR)
+	$(CC) $(CFLAGS) -g -c $< -o $@
+
+clean:
+	rm -rf $(OBJ_DIR)/*.o
+	$(MAKE) clean -C $(LIBFT_DIR)
+
+fclean: clean
+	rm -f $(NAME) $(LIBFT)
+
+re: fclean all
+
+#My own
+TEST_DIR = tests
+TEST_SRCS = $(wildcard $(TEST_DIR)/*.c)
+TEST_BINS = $(TEST_SRCS:$(TEST_DIR)/%.c=$(BIN_DIR)/%)
+NAME_COMPARE = $(BIN_DIR)/test_printf_vs_ft_printf_tester
+
+# Definindo algumas cores
+RED    := \033[0;31m
+GREEN  := \033[0;32m
+YELLOW := \033[0;33m
+NC     := \033[0m  # Sem cor (reset)
+B_RED    := \033[1;31m
+B_GREEN  := \033[1;32m
+B_YELLOW := \033[1;33m
+
+tests: $(TEST_BINS)
+	@echo "Todos os testes foram compilados."
+
+$(BIN_DIR)/%: $(TEST_DIR)/%.c $(NAME)
+	$(CC) $(CFLAGS) -g $< $(NAME) $(LIBFT) -o $@_tester
+
+compare: tests
+	@echo "$(B_YELLOW)Iniciando o comparativo$(NC)"
+	./$(NAME_COMPARE)
+
+$(NAME_COMPARE): $(SRCS_COMPARE)
+	@echo "$(B_YELLOW)Compilando o arquivo $(SRCS_COMPARE)$(NC)"
+	$(CC) $(CFLAGS) -g $(SRCS_COMPARE) -o $(NAME_COMPARE)
+
+cleantestes:
+	rm -f $(BIN_DIR)/*tester
+
+.PHONY: all clean fclean re
+
---
+++ include/ft_printf.h
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marcudos <marcudos@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/22 19:21:19 by marcudos          #+#    #+#             */
+/*   Updated: 2024/11/27 20:12:16 by marcudos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft.h"
+# define SPECIFIERS "cspdiuxX%"
+# define FLAGS "-0#+ "
+
+typedef struct s_specifier
+{
+	char	conversion;
+	int		flag_minus;
+	int		flag_zero;
+	int		flag_hash;
+	int		flag_space;
+	int		flag_plus;
+	int		width;
+	int		precision;
+	int		is_precision_specified;
+}	t_spec;
+
+void	parse_format(const char *format, t_list **lst_tokens);
+void	initialize_specifier(t_spec *spec);
+void	parse_flags(const char *format, int *i, t_spec *spec);
+void	parse_width(const char *format, int *i, t_spec *spec);
+void	parse_precision(const char *format, int *i, t_spec *spec);
+void	parse_conversion(const char *format, int *i, t_spec *spec);
+void	free_specifiers(t_list *lst_tokens);
+void	del_specifier(void *content);
+t_spec	*parse_specifier(const char *format, int *i);
+int		ft_printf(const char *format, ...);
+int		next_letter(const char *format, int i);
+int		print_format(const char *format, t_list *lst_tokens, va_list args);
+int		process_token(t_spec *spec, va_list args);
+int		print_char(t_spec *spec, va_list args);
+int		print_string(t_spec *spec, va_list args);
+int		print_integer(t_spec *spec, va_list args);
+int		print_unsigned(t_spec *spec, va_list args);
+int		print_hex(t_spec *spec, va_list args);
+int		print_pointer(t_spec *spec, va_list args);
+int		print_percent(t_spec *spec);
+char	define_pad(t_spec *spec);
+int		print_width_padding(int width, char pad_char);
+char	*convert_to_hex(unsigned long value, int uppercase);
+char	*hex_uppercase(int uppercase);
+int		print_null_pointer(t_spec *spec);
+int		print_pointer_with_width(t_spec *spec, char *hex_str);
+int		print_hex_with_width(t_spec *spec, char *hex_str);
+
+#endif // !FT_PRINTF_H
